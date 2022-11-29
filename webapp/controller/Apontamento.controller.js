8966a220246f16c778a2a9b05bf55ce7b5bebec1
evandrolyrio <evandrofreitas94@gmail.com>
2022-11-29 10:12:01 -0300

    [Evandro Lyrio] 29/11/2022

---
+++ webapp/controller/Apontamento.controller.js
@@ -0,0 +1,121 @@
+sap.ui.define([
+	"Apontamento/paZPP_APONTAMENTO/controller/BaseController"
+	"sap/ui/model/json/JSONModel",
+	"sap/ndc/BarcodeScanner",
+	"sap/m/MessageBox"	
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("Apontamento.paZPP_APONTAMENTO.controller.Apontamento", {
+
+	    onInit: function() {
+
+			this.setModel(this.getOwnerComponent().getModel());
+
+			this.setModel(new JSONModel({
+				busy: false,
+				//FilterData
+				PassagemSet: [],
+				Aufnr: ""
+			}), "viewModel");
+			var that = this;
+			// this._currentContext = this.getSource().getBindingContext();
+			this.oDialog = new sap.ui.xmlfragment("Apontamento.paZPP_APONTAMENTO.view.fragment.DisplayAponDialog", this);
+			if (this.oDialog) {
+				this.getView().addDependent(this.oDialog);
+
+				this.oDialog.setModel(this.getModel());
+				this.oDialog.setModel(new JSONModel({
+					Aufnr: ""
+				}, "dialog"));
+
+				this.oDialog.setBindingContext(this._currentContext);
+				// this.oDialog.setBindingContext(that);
+				this.oDialog.open();
+			}			
+		},
+		goToPass: function(oEvent) {
+			var oDialogData = this.oDialog.getModel().getData();
+			var that = this;
+			this.oDialog.close();
+			this.oDialog.destroy(true);
+			
+			var oModel = this.getModel();
+
+			this.getModel("viewModel").setProperty("/busy", true);
+			oModel.invalidate();
+			oModel.callFunction("/GetPassagem", {
+				method: "GET",
+				urlParameters: {
+					Aufnr: oDialogData.Aufnr
+				},
+				success: function(oData) {
+					if (!oData.Barcode) {
+						MessageBox.information("Não foi possível localizar a ordem informada.");
+						that.getModel("viewModel").setProperty("/busy", false);
+						that.navigateBack();
+					} else {
+						that.getModel("viewModel").setProperty("/Aufnr", oData.Barcode);
+					    that.getModel("viewModel").setProperty("/busy", false);
+					    // that.oDialog.close();
+						// that.oDialog.destroy(true);
+						that.scanHU().then(function (scanned) {
+							var barcode = scanned;
+							var oModel2 = that.getModel();
+							oModel2.invalidate();
+							oModel2.callFunction("/Transferencia", {
+								method: "GET",
+								urlParameters: {
+									User: 'N',
+									Barcode: barcode,
+									Aufnr: that.getModel("viewModel").getProperty("/Aufnr")
+								},
+								success: function(Data) {	
+									that.getModel("viewModel").setProperty("/PassagemSet", Data.results);
+									that.getModel("viewModel").setProperty("/busy", false);
+									that.getView().byId("tbPassagem").getBinding("items").refresh();
+									that.lerCod();
+								},
+								error: function(error) {
+									that.getModel("viewModel").setProperty("/busy", false);
+								}
+							});	
+						});																
+					}
+				},
+				error: function(error) {
+					// alert(this.oResourceBundle.getText("ErrorReadingProfile"));
+					// oGeneralModel.setProperty("/sideListBusy", false);
+					MessageBox.information("Erro");
+					that.getModel("viewModel").setProperty("/busy", false);
+				}
+			});
+		},		
+		lerCod: function() {
+			var that = this;
+			this.scanHU().then(function (scanned) {
+				var barcode = scanned;
+				var oModel = that.getModel();
+				oModel.invalidate();
+				oModel.callFunction("/Transferencia", {
+					method: "GET",
+					urlParameters: {
+						User: 'O',
+						Barcode: barcode,
+						Aufnr: that.getModel("viewModel").getProperty("/Aufnr")
+					},
+					success: function(oData) {	
+						that.getModel("viewModel").setProperty("/PassagemSet", oData.results);
+						that.getModel("viewModel").setProperty("/busy", false);
+						that.getView().byId("tbMontaKIT").getBinding("items").refresh();
+						that.lerCod();
+					},
+					error: function(error) {
+						that.getModel("viewModel").setProperty("/busy", false);
+					}
+				});	
+			});					
+		}
+
+	});
+});
